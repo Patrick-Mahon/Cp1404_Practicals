61b352221620b8441f840382fa35cb907171f2d2
Patrick-Mahon <patrick.mahon@my.jcu.edu.au>
2017-08-31 11:52:03 +1000

    prac_05 finished

--- prac_05/word_counter.py
+++ prac_05/word_counter.py
@@ -2,3 +2,17 @@
 word_list = {}
 word_string = input("Enter string: ")
 
+words = word_string.split()
+for word in words:
+    amount = word_list.get(word, 0)
+    if amount is None:
+        word_list[word] = 1
+    else:
+        word_list[word] = amount + 1
+
+words = list(word_list.keys())
+words.sort()
+max_length = max((len(word) for word in words))
+for word in words:
+    print("{:{}} : {}".format(word, max_length, word_list[word]))
+
